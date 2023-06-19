400eff65af9f9ccd846db596e13518d382fec421
Peter Zhu <peter@peterzhu.ca>
2023-06-19 16:44:53 -0400

    Add latest_gc_info to GCContext

--- lib/autotuner/gc_context.rb
+++ lib/autotuner/gc_context.rb
@@ -2,16 +2,18 @@
 
 module Autotuner
   class GCContext
-    attr_reader :stat, :stat_heap
+    attr_reader :stat, :stat_heap, :latest_gc_info
 
     def initialize
       @stat = GC.stat
       @stat_heap = GC.stat_heap
+      @latest_gc_info = GC.latest_gc_info
     end
 
     def update
       GC.stat(@stat)
       GC.stat_heap(nil, @stat_heap)
+      GC.latest_gc_info(@latest_gc_info)
     end
   end
 end
--- test/autotuner/test_gc_context.rb
+++ test/autotuner/test_gc_context.rb
@@ -11,16 +11,19 @@ module Autotuner
     def test_update_does_not_change_hash
       orig_stat = @gc_context.stat
       orig_stat_heap = @gc_context.stat_heap
+      orig_latest_gc_info = @gc_context.latest_gc_info
 
       @gc_context.update
 
       assert_equal(orig_stat, @gc_context.stat)
       assert_equal(orig_stat_heap, @gc_context.stat_heap)
+      assert_equal(orig_latest_gc_info, @gc_context.latest_gc_info)
     end
 
     def test_update_does_not_change_keys
       orig_stat_keys = @gc_context.stat.keys
       orig_stat_heap_keys = @gc_context.stat_heap.keys
+      orig_latest_gc_info_keys = @gc_context.latest_gc_info.keys
 
       @gc_context.update
 
@@ -28,14 +31,18 @@ module Autotuner
       assert_equal(orig_stat_keys, @gc_context.stat.keys)
       refute_empty(orig_stat_heap_keys)
       assert_equal(orig_stat_heap_keys, @gc_context.stat_heap.keys)
+      refute_empty(orig_latest_gc_info_keys)
+      assert_equal(orig_latest_gc_info_keys, @gc_context.latest_gc_info.keys)
     end
 
     def test_update_updates_hashes
       stat_hash = @gc_context.stat
       stat_heap_hash = @gc_context.stat_heap
+      latest_gc_info_hash = @gc_context.latest_gc_info
 
       GC.expects(:stat).with(stat_hash).once
       GC.expects(:stat_heap).with(nil, stat_heap_hash).once
+      GC.expects(:latest_gc_info).with(latest_gc_info_hash).once
 
       @gc_context.update
     end
