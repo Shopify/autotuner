54cba98e7430796f2bd604e7e5b8f1dd21bfd43e
Peter Zhu <peter@peterzhu.ca>
2023-09-20 15:57:08 -0400

    Check for GC.stat_heap in GCContext
    Ruby 3.1 and earlier does not have GC.stat_heap, so we should check
    that it exists before using it.

--- lib/autotuner/gc_context.rb
+++ lib/autotuner/gc_context.rb
@@ -2,18 +2,30 @@
 
 module Autotuner
   class GCContext
-    attr_reader :stat, :stat_heap, :latest_gc_info
+    HAS_STAT_HEAP = GC.respond_to?(:stat_heap)
+
+    attr_reader :stat, :latest_gc_info
+
+    if HAS_STAT_HEAP
+      attr_reader :stat_heap
+    end
 
     def initialize
       @stat = GC.stat
-      @stat_heap = GC.stat_heap
       @latest_gc_info = GC.latest_gc_info
+
+      if HAS_STAT_HEAP
+        @stat_heap = GC.stat_heap
+      end
     end
 
     def update
       GC.stat(@stat)
-      GC.stat_heap(nil, @stat_heap)
       GC.latest_gc_info(@latest_gc_info)
+
+      if HAS_STAT_HEAP
+        GC.stat_heap(nil, @stat_heap)
+      end
     end
   end
 end
--- test/autotuner/test_gc_context.rb
+++ test/autotuner/test_gc_context.rb
@@ -6,43 +6,44 @@ module Autotuner
   class TestGCContext < Minitest::Test
     def setup
       @gc_context = GCContext.new
+      @has_stat_heap = RUBY_VERSION >= "3.2"
     end
 
     def test_update_does_not_change_hash
       orig_stat = @gc_context.stat
-      orig_stat_heap = @gc_context.stat_heap
       orig_latest_gc_info = @gc_context.latest_gc_info
+      orig_stat_heap = @gc_context.stat_heap if @has_stat_heap
 
       @gc_context.update
 
       assert_equal(orig_stat, @gc_context.stat)
-      assert_equal(orig_stat_heap, @gc_context.stat_heap)
       assert_equal(orig_latest_gc_info, @gc_context.latest_gc_info)
+      assert_equal(orig_stat_heap, @gc_context.stat_heap) if @has_stat_heap
     end
 
     def test_update_does_not_change_keys
       orig_stat_keys = @gc_context.stat.keys
-      orig_stat_heap_keys = @gc_context.stat_heap.keys
       orig_latest_gc_info_keys = @gc_context.latest_gc_info.keys
+      orig_stat_heap_keys = @gc_context.stat_heap.keys if @has_stat_heap
 
       @gc_context.update
 
       refute_empty(orig_stat_keys)
       assert_equal(orig_stat_keys, @gc_context.stat.keys)
-      refute_empty(orig_stat_heap_keys)
-      assert_equal(orig_stat_heap_keys, @gc_context.stat_heap.keys)
       refute_empty(orig_latest_gc_info_keys)
       assert_equal(orig_latest_gc_info_keys, @gc_context.latest_gc_info.keys)
+      refute_empty(orig_stat_heap_keys) if @has_stat_heap
+      assert_equal(orig_stat_heap_keys, @gc_context.stat_heap.keys) if @has_stat_heap
     end
 
     def test_update_updates_hashes
       stat_hash = @gc_context.stat
-      stat_heap_hash = @gc_context.stat_heap
       latest_gc_info_hash = @gc_context.latest_gc_info
+      stat_heap_hash = @gc_context.stat_heap if @has_stat_heap
 
       GC.expects(:stat).with(stat_hash).once
-      GC.expects(:stat_heap).with(nil, stat_heap_hash).once
       GC.expects(:latest_gc_info).with(latest_gc_info_hash).once
+      GC.expects(:stat_heap).with(nil, stat_heap_hash).once if @has_stat_heap
 
       @gc_context.update
     end
